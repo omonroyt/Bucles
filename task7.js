259c5489e615c775d9cdceb4cd065efde90a2a27
Monroy <monroytellez.oscar@gmail.com>
2025-12-15 15:18:29 -0600

    Task 7

--- task7.js
+++ task7.js
@@ -1,14 +1,34 @@
-// Refer to Task 7 in your Instructions to complete this task
 
-let buzzWords = [
-    "Fizz",
-    "Buzz",
-    "Woof",
-    "Bark",
-    "Awoo",
-    "Bang"
-  ];
-  
-  for (let i = 0; i < 1; i++) {
-    console.log("This is Task Seven!");
-  };
+const prompt = require("prompt-sync")();
+const cantidad = Number (prompt("Ingresa la cantidad de números que quieres que se generen!: ")) ;
+
+let nums= Array.from({ length: cantidad })
+for(let i=0; i < cantidad; i++){
+    nums[i] = i +  1
+    if(nums[i]%3===0 && nums[i]%5===0 && nums[i]%7===0 && nums[i]%11===0){
+      console.log("FizzBuzzWoofBark")
+    }
+    else{
+      if(nums[i]%3===0 && nums[i]%5===0 && nums[i]%7===0){
+          console.log("FizzBuzzWoof");
+        } else{
+          if(nums[i]%3===0 && nums[i]%5===0){
+            console.log("FizzBuzz");
+        } else {
+          if(nums[i]%3===0){
+            console.log("Fizz");
+          } 
+          else if(nums[i]%5===0){
+            console.log("Buzz");
+          } else if(nums[i]%7===0){
+            console.log("Woof");
+          } else if(nums[i]%11===0){
+            console.log("Bark");
+          }
+          else{
+          console.log(nums[i]);
+        }
+        }
+        } 
+    }
+}
